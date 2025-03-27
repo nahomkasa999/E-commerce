692d3557dde0d5a73faa815efce75ed418045495
Nahom-Kasa <nahomkasa345@gmail.com>
2025-03-27 07:10:02 -0400

    Deployment bud fixed

--- config/db.js
+++ config/db.js
@@ -2,19 +2,20 @@
 
 import mongoose from "mongoose";
 
+let cached = global.mongoose;
+
 if(!cached){
     cached = global.mongoose = {conn: null, promise: null}
 }
 
-async function  connectDB() {
+async function connectDB() {
     if(cached.conn){
         return cached.conn
     }
 
     if(!cached.promise){
         const opts = {
-            bufferCommands:false
-
+            bufferCommands: false
         }
         cached.promise = mongoose.connect(`${process.env.MONGODB_URI}/quickcart`, opts).then(mongoose => {return mongoose})
     }
--- models/user.js
+++ models/user.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose"
 
 const userSchema = new mongoose.Schema({
-    _id: {type: string, required: true},
-    name: {type: string, required: true},
-    email: {type: string, required: true, unique: true},
-    imageUrl: {type : string, required: true},
-    cartItems: {type : Object, default: {}}
+    _id: {type: String, required: true},
+    name: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    imageUrl: {type: String, required: true},
+    cartItems: {type: Object, default: {}}
 }, {minimize: false})
 
 const User =mongoose.models.user || mongoose.model('user', userSchema)
