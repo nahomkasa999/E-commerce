32d5131f11a4224b8eab2f829fab3c4596fe27b8
Nahom-Kasa <nahomkasa345@gmail.com>
2025-03-27 08:25:12 -0400

    modified the inngest.js

--- config/inngest.js
+++ config/inngest.js
@@ -10,7 +10,7 @@ export const inngest = new Inngest({ id: "quickcart-next" });
 export const syncUserCreation = inngest.createFunction({
     id: "sync-user from clerk"
 },
-{evernt: 'clerk/user.created'},
+{event: 'clerk/user.created'},
 
 async ({event}) => {
     const {id, first_name, last_name, email_addresses, image_url} = event.data
