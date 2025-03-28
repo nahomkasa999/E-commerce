70231ed0b1948ea2d5bc3401a32d6e73c44a4d35
Nahom-Kasa <nahomkasa345@gmail.com>
2025-03-28 02:53:55 -0400

    Inngest bug fixed

--- config/inngest.js
+++ config/inngest.js
@@ -7,25 +7,21 @@ export const inngest = new Inngest({ id: "quickcart-next" });
 
 //inngest functioin to save user data to a database
 
-export const syncUserCreation = inngest.createFunction({
-    id: "sync-user from clerk"
-},
-{event: 'clerk/user.created'},
-
-async ({event}) => {
-    const {id, first_name, last_name, email_addresses, image_url} = event.data
-    const userData = {
-        _id:id,
-        email: email_addresses[0].email_address,
-        name: first_name + '' + last_name,
-        imageUrl: image_url
-    
+export const syncUserCreation = inngest.createFunction(
+    { id: "sync-user from clerk" },
+    { event: 'clerk/user.created' },
+    async ({ event }) => {
+        const { id, first_name, last_name, email_addresses, image_url } = event.data;
+        const userData = {
+            _id: id,
+            email: email_addresses[0].email_address,
+            name: first_name + ' ' + last_name,
+            imageUrl: image_url
+        };
+        await connectDB();
+        await User.create(userData);
     }
-    await connectDB()
-    await User.create(userData)
-}
-
-)
+);
 
 
 //Inngest Function to update the database
